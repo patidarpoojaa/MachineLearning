d950a3a3764bca3eac9538dd41259cb859d49568
patidarpoojaa <118817956+patidarpoojaa@users.noreply.github.com>
2023-04-23 01:09:32 +0530

    Create Experiment-02.py

---
+++ Experiment-02.py
@@ -0,0 +1,14 @@
+# Pooja Patidar 0827CI201134  
+#List Comprehension 
+ number_list = [ x for x in range(20) if x % 2 == 0] print(number_list) 
+# Pooja Patidar 0827CI201134  
+#List Comprehension 
+ 
+lettersList = list(map(lambda x: x, 'human')) print(lettersList) 
+# Pooja Patidar 0827CI201134  
+#List Comprehension 
+ 
+num_list = [y for y in range(100) if y % 2 == 0 if y % 5 == 0] print(num_list) 
+# Pooja Patidar 0827CI201134  
+#List Comprehension 
+ obj = ["Even" if i%2==0 else "Odd" for i in range(10)] print(obj)  
